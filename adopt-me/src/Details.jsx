61f136bb91a0de0c38da96639ce3dfc493f4e0b1
sam0000-cmd <yug.gurnani091@gmail.com>
2021-08-16 00:41:12 +0530

    Loading State added

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
@@ -9,7 +9,6 @@ class Details extends Component {
   }
 
   async componentDidMount() {
-      console.log(this.props)
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
@@ -22,18 +21,50 @@ class Details extends Component {
   }
 
   render() {
-    const { animal, breed, city, state, description, name } = this.state;
-    return (
-      <div className="details">
-        <div>
-          <h1>{name}</h1>
-          <h2>{`${animal} - ${breed} - ${city}, ${state}`} </h2>
-          <button>Adopt {name}</button>
-          <p>{description}</p>
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    } else {
+      const { animal, breed, city, state, description, name } = this.state;
+      return (
+        <div className="details">
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed} - ${city}, ${state}`} </h2>
+            <button>Adopt {name}</button>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
 export default withRouter(Details);
+
+// function Details(props) {
+//   const [pets, setPets] = useState([]);
+//   useEffect(() => {
+//     requestDetails();
+//   }, []);
+//   async function requestDetails() {
+//     const res = await fetch(
+//       `http://pets-v2.dev-apis.com/pets?id=${props.match.params.id}`
+//     );
+//     const json = await res.json();
+//     setPets(json.pets[0]);
+//   }
+
+//   const { animal, breed, city, state, description, name } = pets;
+//   return (
+//     <div className="details">
+//       <div>
+//         <h1>{name}</h1>
+//         <h2>{`${animal} - ${breed} - ${city}, ${state}`} </h2>
+//         <button>Adopt {name}</button>
+//         <p>{description}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default withRouter(Details);
