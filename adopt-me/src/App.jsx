c86602bdaa48db1411cd7e4424107a87ca0c3ca5
sam0000-cmd <yug.gurnani091@gmail.com>
2021-08-20 12:04:27 +0530

    added some Tailwind

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -10,11 +10,17 @@ function App() {
   const [theme, setTheme] = useState("darkblue");
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
-      <div className="App">
+      <div
+        className="p-0 m-0"
+        style={{
+          background:
+            "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+        }}
+      >
         <Router>
-          <header>
-            <Link to="/">
-              <h1>Adopt Me!</h1>
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link className="text-6xl text-white hover:text-gray-200" to="/">
+              Adopt Me!
             </Link>
           </header>
           <Switch>
